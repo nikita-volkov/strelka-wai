fe9b615e0a18ef2050efc930fb5233deca7d3c42
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-08 08:15:30 -0500

    Update

--- library/Router/WAI.hs
+++ library/Router/WAI.hs
@@ -5,8 +5,12 @@ import Data.Text (Text)
 import qualified Router.RequestParser as A
 import qualified Router.ResponseBuilder as B
 import qualified Router.Executor as C
+import qualified Router.Model as F
 import qualified Network.Wai as D
 import qualified Network.Wai.Handler.Warp as E
+import qualified Network.HTTP.Types as G
+import qualified Data.CaseInsensitive as H
+import qualified Data.ByteString.Builder as I
 
 
 routeServer :: Monad m => Int -> (forall a. m a -> IO (Either Text a)) -> A.RequestParser m B.ResponseBuilder -> IO ()
@@ -21,7 +25,38 @@ routeApplication runBase route =
   where
     internalError message =
       B.run B.internalErrorStatus
-    routerRequest waiRequest =
-      undefined
-    waiResponse response =
-      undefined
+
+routerRequest :: D.Request -> F.Request
+routerRequest waiRequest =
+  F.Request method path query headers inputStream
+  where
+    method =
+      F.Method (D.requestMethod waiRequest)
+    path =
+      F.Path (D.rawPathInfo waiRequest)
+    query =
+      F.Query (D.rawQueryString waiRequest)
+    headers =
+      map routerHeader (D.requestHeaders waiRequest)
+    inputStream =
+      F.InputStream (D.requestBody waiRequest)
+
+routerHeader :: G.Header -> F.Header
+routerHeader (name, value) =
+  F.Header (H.foldedCase name) (H.foldCase value)
+
+waiResponse :: F.Response -> D.Response
+waiResponse (F.Response status headerList outputStream) =
+  D.responseStream (waiStatus status) (map waiHeader headerList) (waiStreamingBody outputStream)
+
+waiStatus :: F.Status -> G.Status
+waiStatus (F.Status statusCode) =
+  toEnum statusCode
+
+waiHeader :: F.Header -> G.Header
+waiHeader (F.Header name value) =
+  (H.mk name, value)
+
+waiStreamingBody :: F.OutputStream -> D.StreamingBody
+waiStreamingBody (F.OutputStream outputStreamFn) =
+  \consumeBuilder flush -> outputStreamFn (consumeBuilder . I.byteString) flush
